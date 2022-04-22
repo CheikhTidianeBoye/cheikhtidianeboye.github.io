5086c3e863875e5b94e6f4ad0cf456266036a224
CheikhTidianeBoye <cheikhboye10.cb@gmail.com>
2022-04-22 22:07:32 +0200

    changement de portfolio en index

--- web.php
+++ web.php
@@ -1,21 +1,13 @@
 <?php
-$user_name = $_POST["user_name"];
-$user_mail = $_POST["user_mail"];
-$user_message = $_POST["user_message"]; 
-echo "Hello " . $user_name .  ", <br /> 
-You are registered: ";
+if (isset($_POST["ENVOYER"])) {
+  $Email = $_POST["email"];
+  $message = $_POST["message"];
+  if (empty($Email) || empty($message)) {
+     echo  ("Tous les champs doivent etre completé!");
+  }
+  else {
+      echo "courrier envoyé!";
+  }
+}
 ?>
 
-<?php
-$user_name = $_POST["user_name"];
-if($user_name=="")
-   {
-   echo "Le nom n est pas defini";
-   }
-elseif ($user_mail=="") {
-    echo "L'email' n est pas defini";
-}
-else {
-    echo "c correct";
-}
-?>
